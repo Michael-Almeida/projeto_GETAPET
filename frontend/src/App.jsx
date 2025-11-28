c337eca9520e6dd0623950068252ef36ac2ccfdc
Michael Cardoso <michaelcardoso97@gmail.com>
2025-11-28 06:25:05 -0300

    Criação de Footer e Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,19 @@ import Login from "./components/pages/auth/Login";
 import Register from "./components/pages/auth/Register";
 import Home from "./components/pages/Home";
 
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/" element={<Home />}></Route>
       </Routes>
+      <Footer />
     </Router>
   );
 }
