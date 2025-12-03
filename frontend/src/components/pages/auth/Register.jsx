e2100b4133b4505f280b1066e696b301f85b7781
Michael Cardoso <michaelcardoso97@gmail.com>
2025-12-03 08:35:18 -0300

    formulário de cadastro

--- frontend/src/components/pages/auth/Register.jsx
+++ frontend/src/components/pages/auth/Register.jsx
@@ -1,7 +1,49 @@
+import Input from "../../form/Input";
+
+function handleChange(e) {}
+
 function Register() {
   return (
     <section>
       <h1>Register</h1>
+      <form>
+        <Input
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite o seu nome"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Telefone"
+          type="text"
+          name="phone"
+          placeholder="Digite o seu telefone"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite seu e-mail"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite a sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Confirmação de senha"
+          type="password"
+          name="confirmpasswrod"
+          placeholder="Confirme a sua senha"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Cadastrar" />
+      </form>
     </section>
   );
 }
